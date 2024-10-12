9a878e994ac83d47ffc9004e2d574815e59ffecc
Noor Eldeen Salah <nooreldeensalah99@gmail.com>
2024-10-12 03:41:10 +0300

    Support filtering on credentials model fields

--- credentials_dashboard/accounts/views.py
+++ credentials_dashboard/accounts/views.py
@@ -5,7 +5,10 @@ from rest_framework.permissions import IsAuthenticated
 from .models import Credentials
 from .serializers import CredentialsSerializer
 from rest_framework import generics
+from django_filters.rest_framework import DjangoFilterBackend
 
 class CredentialsListView(generics.ListAPIView):
     queryset = Credentials.objects.all()
     serializer_class = CredentialsSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['url', 'username', 'application']
