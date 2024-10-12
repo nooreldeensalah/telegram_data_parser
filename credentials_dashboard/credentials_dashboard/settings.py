40fd0f30a6b124e23475d2d866d39df83df84c60
Noor Eldeen Salah <nooreldeensalah99@gmail.com>
2024-10-12 06:45:28 +0300

    Configure `CORS` in `settings.py`

--- credentials_dashboard/credentials_dashboard/settings.py
+++ credentials_dashboard/credentials_dashboard/settings.py
@@ -38,7 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    'django_filters'
+    'django_filters',
+    'corsheaders'
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
 ]
 
 REST_FRAMEWORK = {
@@ -55,6 +60,7 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
