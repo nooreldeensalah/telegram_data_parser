5fb347b8aea1f12ddb1ae3d319de77f8cdaa77d4
Noor Eldeen Salah <nooreldeensalah99@gmail.com>
2024-10-12 03:38:33 +0300

    Add view for credentials model

--- credentials_dashboard/accounts/views.py
+++ credentials_dashboard/accounts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from .models import Credentials
+from .serializers import CredentialsSerializer
+from rest_framework import generics
 
-# Create your views here.
+class CredentialsListView(generics.ListAPIView):
+    queryset = Credentials.objects.all()
+    serializer_class = CredentialsSerializer
