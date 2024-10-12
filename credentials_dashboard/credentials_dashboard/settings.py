4a7e5a8c22aa7cf8bd78f34aa0d4b5bd71a1b981
Noor Eldeen Salah <nooreldeensalah99@gmail.com>
2024-10-12 06:24:15 +0300

    Use pagination with credentials data

--- credentials_dashboard/credentials_dashboard/settings.py
+++ credentials_dashboard/credentials_dashboard/settings.py
@@ -48,6 +48,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 50
 }
 
 MIDDLEWARE = [
