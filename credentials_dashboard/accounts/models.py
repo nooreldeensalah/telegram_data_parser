8b94759580b26d89e15b876d55c7bcd3c9ebfe53
Noor Eldeen Salah <nooreldeensalah99@gmail.com>
2024-10-12 03:31:09 +0300

    Create `Credentials` database class in `models.py`

--- credentials_dashboard/accounts/models.py
+++ credentials_dashboard/accounts/models.py
@@ -1,3 +1,9 @@
-from django.db import models
+from djongo import models
 
-# Create your models here.
+class Credentials(models.Model):
+    url = models.CharField(max_length=255)
+    username = models.CharField(max_length=255, null=True)
+    password = models.CharField(max_length=255, null=True)
+    application = models.CharField(max_length=255, null=True)
+    class Meta:
+        db_table = "credentials"
