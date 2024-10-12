70aad7d6ea86dd99b8d8ac38abb4ce58693ca95a
Noor Eldeen Salah <nooreldeensalah99@gmail.com>
2024-10-12 06:18:30 +0300

    Correct the collection fields names to match the parsed data

--- credentials_dashboard/accounts/models.py
+++ credentials_dashboard/accounts/models.py
@@ -1,9 +1,10 @@
 from djongo import models
 
 class Credentials(models.Model):
-    url = models.CharField(max_length=255)
-    username = models.CharField(max_length=255, null=True)
-    password = models.CharField(max_length=255, null=True)
-    application = models.CharField(max_length=255, null=True)
+    url = models.CharField(max_length=255, db_column='URL')  # Custom MongoDB field name
+    username = models.CharField(max_length=255, null=True, db_column='Username')  # Custom field
+    password = models.CharField(max_length=255, null=True, db_column='Password')  # Custom field
+    application = models.CharField(max_length=255, null=True, db_column='Application')  # Custom field
+
     class Meta:
         db_table = "credentials"
