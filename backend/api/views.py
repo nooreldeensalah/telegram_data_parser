a269d0e3b028bb6ac6eec2768eeef331603ae629
Noor Eldeen Salah <nooreldeensalah99@gmail.com>
2024-10-24 18:29:08 +0300

    Remove unused imports

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,3 @@
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from rest_framework.response import Response
-from rest_framework import status
 from rest_framework import viewsets
 from .models import Credentials
 from .serializers import CredentialsSerializer
